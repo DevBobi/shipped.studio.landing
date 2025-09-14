74ffd924b9e1496b677cbded03dd598340e31460
sadekxd <sadekirfan3@gmail.com>
2025-09-14 14:58:20 +0400

    Update ESLint configuration to disable specific rules for improved development flexibility.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,13 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
